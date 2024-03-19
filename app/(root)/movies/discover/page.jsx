a63393fa919c8e420ebdd8217733d52c7a2e1240
Collins O. Odhiambo <comon928@gmail.com>
2024-03-19 03:19:42 +0000

    add discover and a bit of trending

---
+++ app/(root)/movies/discover/page.jsx
@@ -0,0 +1,60 @@
+"use client"
+
+import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+import Link from 'next/link'
+
+
+const discoverUrl = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+
+function Discover() {
+
+    const [ApiResponse, setApiResponse] = useState([])
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        // Step 1: Get a request token
+        const response = await axios({
+          method: "GET",
+          url: discoverUrl,
+          headers: {
+            Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+          },
+        });
+
+        setApiResponse(response.data.results);
+        // console.log(response.data.results)
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+
+    getMovies()
+
+  }, [])
+
+  return (
+    <>
+    <main className="flex min-h-screen flex-col items-start md:ml-[210px] md:mr-[210px]  md:mt-[80px]">
+
+<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+       {ApiResponse && ApiResponse.map((movie) => (
+    <div class="grid gap-4">
+        <div key={movie.id}>
+            <img class="h-auto max-w-full rounded-lg" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt=""/>
+        <h2 className='text-center'>{movie.title}</h2>
+        </div>
+   
+</div>
+))}
+</div>
+</main>
+    </>
+  )
+}
+
+export default Discover
--- app/components/MovieList.jsx
+++ app/components/MovieList.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+import Link from 'next/link'
 
-const MovieList = ({ Trendingmovies }) => {
+// const discoverUrl = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+
+// https://api.themoviedb.org/3/trending/movie/day?language=en-US
+
+const MovieList = ({ urlType, imageStyle, trendingTrailerBtn, divStyle, customStyle, limit }) => {
+  const [ApiResponse, setApiResponse] = useState([])
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        // Step 1: Get a request token
+        const response = await axios({
+          method: "GET",
+          url: urlType,
+          headers: {
+            Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+          },
+        });
+
+        setApiResponse(response.data.results);
+        // console.log(response.data.results)
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+
+    getMovies()
+
+  }, [])
   return (
-    <div>
-      {Trendingmovies.map((movie) => (
-        <div key={movie.id}>
-          <h2>{movie.title}</h2>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-        </div>
-      ))}
-     
-    </div>
+    <>
+      <div className={customStyle}>
+        {ApiResponse && ApiResponse.slice(0, limit).map((movie) => (
+
+          <div key={movie.id} className={divStyle ? divStyle : "w-[155px]"}>
+            <img className={imageStyle} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
+            alt={movie.title} width={60} height={10} />
+            <div>
+            <h2 className='text-center'>{movie.title}</h2>
+              {trendingTrailerBtn && trendingTrailerBtn()}
+              </div>
+
+          </div>
+
+        ))}
+      </div>
+    </>
   );
 };
 
