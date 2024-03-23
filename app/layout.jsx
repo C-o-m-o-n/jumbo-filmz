aba4b8c4583e3ef9721451ae66d0f864cebd0ff7
Collins O. Odhiambo <comon928@gmail.com>
2024-03-23 07:32:31 +0000

    ADD FAVICON

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] });
-import Head from 'next/head'
 
 export const metadata = {
   title: "Jumbo filmz",
@@ -12,9 +11,7 @@ export default function RootLayout({ children }) {
   return (
 	   <ClerkProvider>
     <html lang="en">
-    <Head>
-        <link rel="icon" href="/assets/jumbofilmz.jpeg" />
-      </Head>
+
       <body className={inter.className}>
         {children}
         </body>
