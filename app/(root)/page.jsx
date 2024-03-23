e30a3e451ddb3f824c1449cadae4b8ada1af8325
Clifford537 <cliffordmukosh@gmail.com>
2024-03-23 18:51:33 +0300

    updated upcoming url

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
 <section class="h-screen bg-[url('/assets/JusticeLeague.webp')] bg-cover opacity-50 dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-        <Link href="/content/upcoming" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+        <Link href="/content/movies/upcoming" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span class="text-xs bg-[red] rounded-full text-white px-4 py-1.5 mr-3">Upcoming</span> <span class="text-sm font-medium">See movies that are upcoming!</span> 
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </Link>
